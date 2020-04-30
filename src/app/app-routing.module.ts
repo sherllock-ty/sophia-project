becade88d3d7dc039cb5b81a71c6909444fd7caf
Doryuken <aymensamir.sari@univ-tlemcen.dz>
2020-04-30 03:01:29 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/shop/home/home.component';
+import { DemoComponent } from './components/demo/demo.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./components/shop/shop.module').then(m => m.ShopModule)
+      },
+      {
+        path: 'pages',
+        loadChildren: () => import('./components/pages/pages.module').then(m => m.PagesModule)
+
+      },
+      {
+        path: 'blog',
+        loadChildren: () => import('./components/blog/blog.module').then(m => m.BlogModule)
+      },
+    ]
+  },
+  {
+    path: '123',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(appRoutes, {scrollPositionRestoration :'top'})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
